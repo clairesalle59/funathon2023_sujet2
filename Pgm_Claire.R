d9a9bd6982eee0065b26d733a6fdc3ec52dd5bbd
clairesalle59 <claire.salle@agriculture.gouv.fr>
2023-06-27 14:02:42 +0000

    Cartographie des rasters

--- Pgm_Claire.R
+++ Pgm_Claire.R
@@ -154,3 +154,109 @@ carte_parc_prox_html <- leaflet(parc_prox_lib) %>%
 #saveWidget(widget = carte_parc_prox_html, file = "carte_parc_prox.html")
 
 carte_parc_prox_html
+
+
+###### PARTIE 3 : METEO
+
+library(RPostgres)
+library(dplyr)
+library(aws.s3)
+library(ggplot2)
+library(raster)
+library(sf)
+library(janitor)
+library(knitr)
+
+# Pour avoir les noms de dates en français
+invisible(Sys.setlocale("LC_ALL", "fr_FR.UTF-8"))
+
+options(knitr.kable.NA = "")
+
+conn <- dbConnect(Postgres(),
+                 user = Sys.getenv("USER_POSTGRESQL"),
+                 password = Sys.getenv("PASS_POSTGRESQL"),
+                 host = Sys.getenv("HOST_POSTGRESQL"),
+                 dbname = "defaultdb",
+                 port = 5432,
+                 check_interrupts = TRUE)
+
+drias_raster <- s3read_using(
+  function(f) readAll(brick(f)),
+  object = "2023/sujet2/diffusion/resultats/drias.tif",
+  bucket = "projet-funathon",
+  opts = list("region" = ""))
+
+drias_df <- as.data.frame(drias_raster, xy = TRUE) %>% tidyr::drop_na()
+colnames(drias_df) <- c(
+  "x",
+  "y",
+  "NORRRA",
+  "NORSTM6",
+  "NORSTM0",
+  "NORSDA",
+  "NORDATEVEG",
+  "NORDATEDG",
+  "NORDATEPG",
+  "ARRA",
+  "ASTM6",
+  "ASTM0",
+  "ASDA",
+  "ADATEVEG",
+  "ADATEDG",
+  "ADATEPG",
+  "ALTI"
+)
+
+drias_df %>% head(10) %>% kable()
+
+
+####### 3.2 Visualisations du raster 
+
+drias_raster_arra <- s3read_using(
+  function(f) readAll(raster(f, band = 8)),
+  object = "2023/sujet2/diffusion/resultats/drias.tif",
+  bucket = "projet-funathon",
+  opts = list("region" = ""))
+
+# Avec palette custom
+palette <- c("#1457ff", "#3c9aff", "#6dc4ff", "#a8e1ff", "#dff1fb", "#f8e9eb", "#fca8ab", "#f9575d", "#f2060b", "#a20102")
+breaks <- c(-200, -160, -120, -80, -40, -0, 40, 80, 120, 160, 200)
+
+raster::plot(x = drias_raster_arra,
+             col = rev(palette),
+             breaks = breaks,
+             main = "Ecart de cumul de précipitations d'avril à octobre (mm)\nentre 2021-2050 et 1976-2005")
+
+
+###### 3.3 Requêtes PostgreSQL
+
+query <- "
+SELECT *
+FROM drias.previsions
+"
+drias_sf <- st_read(cnx, query = query)
+
+ggplot() + 
+  geom_sf(data = drias_sf, aes(fill = arra), color = NA) +
+  binned_scale(aesthetics = "fill", scale_name = "custom", 
+               palette = ggplot2:::binned_pal(scales::manual_pal(values = rev(palette)[-1])),
+               guide = "bins",
+               breaks = breaks)
+
+###### 3.4 Appariement spatial entre données DRIAS et RPG
+
+# On récupère par carreau de la grille DRIAS la surface pour chaque type de culture
+query <- "
+SELECT B.point, code_cultu, Sum(surf_parc) AS surface, B.arra
+FROM rpg.parcelles AS A
+JOIN drias.previsions AS B
+ON ST_Intersects(A.geom , B.geometry)
+GROUP BY B.point, B.arra, code_cultu
+"
+res <- dbSendQuery(conn, query)
+arra_df <- dbFetch(res)
+
+arra_df %>% head(10) %>% kable()
+
+
+#######   3.5 Calcul d’indicateurs par type de culture
